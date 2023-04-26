9aaa03739e27a56f3eec4d1599b851e15e9ee349
Giovanna Mendonça Federico <106484465+giih06@users.noreply.github.com>
2023-04-26 17:11:06 -0300

    area, diagonal e perimetro de um retangulo com programaçao orientada a objetos

---
+++ seção 8/Retangulo/src/entities/Rectangle.java
@@ -0,0 +1,22 @@
+package entities;
+
+public class Rectangle {
+
+    //atributos
+    public double width;
+    public double height;
+
+    //métodos
+    public double area() {
+        return width * height;
+    }
+
+    public double perimeter() {
+        return 2 * (width + height);
+    }
+
+    public double diagonal() {
+        return Math.sqrt(width * width + height * height);
+    }
+
+}
