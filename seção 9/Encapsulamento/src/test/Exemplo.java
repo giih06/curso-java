f8c0d19115b06a2defc102d769c58a391eb33daf
Giovanna Mendonça Federico <106484465+giih06@users.noreply.github.com>
2023-05-01 21:40:31 -0300

    Encapsulamento exemplo

---
+++ seção 9/Encapsulamento/src/test/Exemplo.java
@@ -0,0 +1,28 @@
+package test;
+public class Exemplo {
+
+    /* Padrão para implementação de getters e setters */
+
+    /* a palavra "private" serve para dizer que esses atributos não podem ser acessados por outras classes */
+    private String name;
+    private double price;
+
+    /* Caso eu precise acessar o nome do produto ou alterar o nome do produto, precisarei fazer métodos para isso. Por padrão, o método responsável por obter o atributo vai ser o método que vai ter o nome de get seguido do nome do atributo (em letra Maiuscula) */
+    public String getName() {
+    return name;
+    }
+
+    /* Caso eu precise alterar o valor do meu atributuo, precisarei usar o método set seguido do nome do atributo (em letra Maiúscula). Como esse método é para alterar o valor do atributo, ele não irá retornar nada (void) e terá que receber um valor para o atributo (String name). Recebendo esse valor, o método então vai atribuir para o atributo do objeto o valor recebido */
+    public void setName(String name) {
+    this.name = name;
+    }
+
+    public double getPrice() {
+    return price;
+    }
+
+    public void setPrice(double price) {
+    this.price = price;
+    }
+
+}
