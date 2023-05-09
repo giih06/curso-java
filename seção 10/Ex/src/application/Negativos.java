45cfce803796bab31941a633cb78472e452ef35c
Giovanna Mendonça Federico <106484465+giih06@users.noreply.github.com>
2023-05-09 20:57:03 -0300

    criando codigo de vetores da classe negativos.java

--- seção 10/Ex/src/application/Negativos.java
+++ seção 10/Ex/src/application/Negativos.java
@@ -1,6 +1,30 @@
 package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Negativos {
+    /* Faça um programa que leia um número inteiro positivo N (máximo = 10) e depois N números inteiros e armazene-os em um vetor. Em seguida, mostrar na tela todos os números negativos lidos. 
+ */
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Quantos números você vai digitar?: ");
+        int n = sc.nextInt();
+
+        double[] vect = new double[n];
+
+        for(int i=0; i<n; i++) {
+            System.out.println("Digite um numero: ");
+            vect[i] = sc.nextInt();
+        }
+
+        
+
+
+
+        sc.close();
+
     }
 }
