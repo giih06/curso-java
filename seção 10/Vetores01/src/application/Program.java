779d93ef27c798d04da98476f4994b00c33633ed
Giovanna Mendonça Federico <106484465+giih06@users.noreply.github.com>
2023-05-08 23:02:31 -0300

    terminei o vetor 01

--- seção 10/Vetores01/src/application/Program.java
+++ seção 10/Vetores01/src/application/Program.java
@@ -1,6 +1,29 @@
 package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        // double = vetor de double, [] indica q estou declarando um vetor, vect = nome da variável, new, double = tipo dos elementos do vetor, [] = tamanho do vetor.
+        double[] vect = new double[n];
+
+        for (int i=0; i<n; i++) {
+            vect[i] = sc.nextDouble();
+        }
+
+        double sum = 0;
+        for (int i=0; i<n; i++)  {
+            sum += vect[i];
+        }
+        double avg = sum / n;
+
+        System.out.printf("AVERAGE HEIGHT: %.2f", avg);
+
+        sc.close();
     }
 }
