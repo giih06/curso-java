cfeeb9a5a59618c369925c5f8d0dc00cd6dd50bd
Giovanna Federico <giovannamfederico@gmail.com>
2023-06-07 23:07:45 -0300

    herança - sistema bancário

---
+++ seção 14 - Herança e Polimofirsmo/sistema_banco_heranca/src/entities/Account.java
@@ -0,0 +1,55 @@
+package entities;
+
+public class Account { // superclasse
+    // atributos
+    private Integer number;
+    private String holder;
+    protected double balance; // pode ser acessado por subclasses e n é público
+
+    // Construtores
+    public Account() {
+    }
+
+    public Account(Integer number, String holder, double balance) {
+        this.number = number;
+        this.holder = holder;
+        this.balance = balance;
+    }   
+
+    // Getters e setters
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public String getHolder() {
+        return holder;
+    }
+
+    public void setHolder(String holder) {
+        this.holder = holder;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    // metodos
+    public void withdraw(double amount) {
+        balance += amount;
+    }
+
+    public void deposit(double amount) {
+        balance -= amount;
+    }
+
+    
+
+}
