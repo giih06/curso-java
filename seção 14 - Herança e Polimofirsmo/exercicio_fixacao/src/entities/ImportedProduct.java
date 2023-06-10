43fba14d61863431a2eda6e2bbb09b3486b6c7e2
Giovanna Federico <giovannamfederico@gmail.com>
2023-06-10 14:05:55 -0300

    exercicio de fixação

---
+++ seção 14 - Herança e Polimofirsmo/exercicio_fixacao/src/entities/ImportedProduct.java
@@ -0,0 +1,42 @@
+package entities;
+
+public final class ImportedProduct extends Product {
+    // atributos
+    private Double customsFee;
+
+    // construtores
+    public ImportedProduct() {
+        super();
+    }
+
+    public ImportedProduct(String name, Double price, Double customsFee) {
+        super(name, price);
+        this.customsFee = customsFee;
+    }
+
+    // Getters e setters
+    public Double getCustomsFee() {
+        return customsFee;
+    }
+
+    public void setCustomsFee(Double customsFee) {
+        this.customsFee = customsFee;
+    }
+
+    // métodos
+    
+    @Override // método sobreposto
+    public final String priceTag() {
+        return super.priceTag() + "(Customs fee: $ " + getCustomsFee() + ")";
+    }
+     
+
+    
+    
+
+
+
+
+
+
+}
---
+++ seção 14 - Herança e Polimofirsmo/exercicio_fixacao/src/entities/UsedProduct.java
@@ -0,0 +1,41 @@
+package entities;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public final class UsedProduct extends Product{
+    // atributo
+    private LocalDate manufactureDate;
+
+    // construtores
+    public UsedProduct() {
+        super();
+    }
+
+    public UsedProduct(String name, Double price, LocalDate manufactureDate) {
+        super(name, price);
+        this.manufactureDate = manufactureDate;
+    }
+    
+    // getters e setters
+    public LocalDate getManufactureDate() {
+        return manufactureDate;
+    }
+
+    public void setManufactureDate(LocalDate manufactureDate) {
+        this.manufactureDate = manufactureDate;
+    }
+
+    // métodos
+
+    @Override // método sobreposto
+    public final String priceTag() {
+        return getName() + "(used) $ " + String.format("%.2f", getPrice()) + " (Manufacture date: " + manufactureDate.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")) + ")";
+    }
+
+    
+
+    
+
+
+}
