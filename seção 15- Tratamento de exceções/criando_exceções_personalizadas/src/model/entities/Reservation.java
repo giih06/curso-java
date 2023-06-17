69502ef9e608cd4f3c365e66435daa728ae9fbae
Giovanna Federico <giovannamfederico@gmail.com>
2023-06-17 17:43:19 -0300

    versao 1 - muito ruim

--- seção 15- Tratamento de exceções/criando_exceções_personalizadas/src/model/entities/Reservation.java
+++ seção 15- Tratamento de exceções/criando_exceções_personalizadas/src/model/entities/Reservation.java
@@ -1,5 +1,55 @@
 package model.entities;
 
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+import java.util.concurrent.TimeUnit;
+import java.util.zip.CheckedInputStream;
+
 public class Reservation {
-    
+    // atributos
+    private Integer roomNumber;
+    private Date checkIn;
+    private Date checkOut;
+
+    private static SimpleDateFormat sdf = new SimpleDateFormat("ddd/MM/yyy");
+
+    // construtor
+    public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
+        this.roomNumber = roomNumber;
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+
+    // getters e setters
+    public Integer getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(Integer roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public Date getCheckIn() {
+        return checkIn;
+    }
+
+    public Date getCheckOut() {
+        return checkOut;
+    }
+
+    // metodos
+    public long duration() {
+        long diff = checkOut.getTime() - checkIn.getTime(); //diferença entre as duas datas em milissegundos
+        return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS); // converte milissegundos para dias
+    }
+
+    public void updateDates(Date checkIn, Date checkOut) {
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+
+    @Override
+    public String toString() {
+        return "Room " + roomNumber + ", check-in: " + sdf.format(checkIn) + ", check-out: " + sdf.format(checkOut) + ", " + duration() + " nights";
+    }
 }
