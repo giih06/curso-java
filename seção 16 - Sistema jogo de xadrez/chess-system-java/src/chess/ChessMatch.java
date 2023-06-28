3ea3b9aa7830c1d895324479113ce4fee9fd593b
Giovanna Federico <giovannamfederico@gmail.com>
2023-06-28 01:11:12 -0300

    Fix - better treatment to promotion

--- seção 16 - Sistema jogo de xadrez/chess-system-java/src/chess/ChessMatch.java
+++ seção 16 - Sistema jogo de xadrez/chess-system-java/src/chess/ChessMatch.java
@@ -1,6 +1,5 @@
 package chess;
 
-import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -131,7 +130,7 @@ public class ChessMatch {
 			throw new IllegalStateException("There is no piece to be promoted");
 		}
 		if (!type.equals("B") && !type.equals("N") && !type.equals("R") & !type.equals("Q")) {
-			throw new InvalidParameterException("Invalid type for promotion");
+			return promoted;
 		}
 
 		Position pos = promoted.getChessPosition().toPosition();
@@ -152,7 +151,7 @@ public class ChessMatch {
 		if (type.equals("Q")) return new Queen(board, color);
 		return new Rook(board, color);
 	}
-    
+
     // make the move
     private Piece makeMove(Position source, Position target) {
         ChessPiece p = (ChessPiece)board.removePiece(source); // remove the origin piece
