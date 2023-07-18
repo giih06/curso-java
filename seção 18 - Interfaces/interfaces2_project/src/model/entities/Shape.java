ecdbd3f547f18b179660a776bcfb7e5f3fc79c2a
Giovanna Federico <giovannamfederico@gmail.com>
2023-07-18 18:02:51 -0300

    comentario da ultima aula

--- seção 18 - Interfaces/interfaces2_project/src/model/entities/Shape.java
+++ seção 18 - Interfaces/interfaces2_project/src/model/entities/Shape.java
@@ -1,6 +1,6 @@
 package model.entities;
 
 public interface Shape {
-
+    // interfaces is already publiic and abstract, so is not necessary to write "public abstract" in front of double area()
 	double area();
 }
