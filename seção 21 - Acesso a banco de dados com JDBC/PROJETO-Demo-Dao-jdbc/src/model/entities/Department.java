1f6ab4735b71f155808ff8fde720c3b02aa07153
Giovanna Federico <giovannamfederico@gmail.com>
2023-08-11 20:57:34 -0300

    Seller entity class

--- seção 21 - Acesso a banco de dados com JDBC/PROJETO-Demo-Dao-jdbc/src/model/entities/Department.java
+++ seção 21 - Acesso a banco de dados com JDBC/PROJETO-Demo-Dao-jdbc/src/model/entities/Department.java
@@ -67,10 +67,4 @@ public class Department implements Serializable {
     public String toString() {
         return "Department [id=" + id + ", name=" + name + "]";
     }
-
-    
-    
-    
-    
-
 }
