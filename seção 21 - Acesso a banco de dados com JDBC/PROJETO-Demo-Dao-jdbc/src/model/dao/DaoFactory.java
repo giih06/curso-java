3fa2f740e50bada13dc78576f4b60e97ecd1797c
Giovanna Federico <giovannamfederico@gmail.com>
2023-08-11 22:34:13 -0300

    SellerDaoJDBC  and DaoFactory

---
+++ seção 21 - Acesso a banco de dados com JDBC/PROJETO-Demo-Dao-jdbc/src/model/dao/DaoFactory.java
@@ -0,0 +1,12 @@
+package model.dao;
+
+import model.dao.imp.SellerDaoJDBC;
+
+// Essa classe terá operações estáticas para instanciar os Daos
+public class DaoFactory {
+
+    // A classe vai expor o método que retorna o tipo da interface, mas internamente ela vai estar instanciando uma implementação ( macete para não precisar expor a implementação, deixa somente a interface)
+    public static SellerDao createSellerDao() {
+        return new SellerDaoJDBC();
+    }
+}
---
+++ seção 21 - Acesso a banco de dados com JDBC/PROJETO-Demo-Dao-jdbc/src/model/dao/imp/SellerDaoJDBC.java
@@ -0,0 +1,42 @@
+package model.dao.imp;
+
+import java.util.List;
+
+import model.dao.SellerDao;
+import model.entities.Department;
+import model.entities.Seller;
+
+// Essa classe é uma implementação JDBC do SellerDao
+public class SellerDaoJDBC implements SellerDao {
+
+    @Override
+    public void insert(Seller obj) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'insert'");
+    }
+
+    @Override
+    public void update(Seller obj) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'update'");
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteById'");
+    }
+
+    @Override
+    public Department findById(Integer id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findById'");
+    }
+
+    @Override
+    public List<Seller> findAll() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findAll'");
+    }
+    
+}
