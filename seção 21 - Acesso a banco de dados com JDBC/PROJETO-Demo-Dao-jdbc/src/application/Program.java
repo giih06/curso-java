2f007b3214c5c867485d70ca2b9c7b52f6b94364
Giovanna Federico <giovannamfederico@gmail.com>
2023-08-12 12:52:50 -0300

    delete implementation

--- seção 21 - Acesso a banco de dados com JDBC/PROJETO-Demo-Dao-jdbc/src/application/Program.java
+++ seção 21 - Acesso a banco de dados com JDBC/PROJETO-Demo-Dao-jdbc/src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -10,6 +11,9 @@ import model.entities.Seller;
 
 public class Program {
     public static void main(String[] args) throws Exception {
+
+        Scanner sc = new Scanner(System.in);
+
         // Instanciar um objeto dessa forma, o programa não conhece a implementação, mas sim a interface. Essa é uma forma de fazer uma ingeção de depenência sem explicitar a implementação
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
@@ -40,6 +44,13 @@ public class Program {
         seller.setName("Martha Waine");
         sellerDao.update(seller);
         System.out.println("Update complete");
-        
+
+        System.out.println("\n=== TESTE 6: seller delete ===");
+        System.out.println("Enter id for delete test: ");
+        int id = sc.nextInt();
+        sellerDao.deleteById(id);
+        System.out.println("Delete completed");
+
+        sc.close();
     }
 }
