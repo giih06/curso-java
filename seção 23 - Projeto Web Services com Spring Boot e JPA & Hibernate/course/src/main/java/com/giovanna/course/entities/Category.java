46612e2b9c3cbf3d82d0b796a0b7d6b203b96770
Giovanna Federico <giovannamfederico@gmail.com>
2023-08-14 12:02:26 -0300

    Many-to-many association with JoinTable

--- seção 23 - Projeto Web Services com Spring Boot e JPA & Hibernate/course/src/main/java/com/giovanna/course/entities/Category.java
+++ seção 23 - Projeto Web Services com Spring Boot e JPA & Hibernate/course/src/main/java/com/giovanna/course/entities/Category.java
@@ -4,12 +4,15 @@ import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
+
 // Entitade do sistama gerenciado pela JPA = @entity em cima da classe
 @Entity
 @Table(name = "tb_category") // nome da tabela
@@ -22,7 +25,8 @@ public class Category implements Serializable{
 
     // associações
     // de 1 categoria para muitos produtos
-    @Transient
+    @JsonIgnore
+	@ManyToMany(mappedBy = "categories")
 	private Set<Product> products = new HashSet<>();
 
     // construtores
