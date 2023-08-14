e283fe9b637776db9bd1b153467917603a5543a5
Giovanna Federico <giovannamfederico@gmail.com>
2023-08-14 10:21:45 -0300

    OrderStatus enum

--- seção 23 - Projeto Web Services com Spring Boot e JPA & Hibernate/course/src/main/java/com/giovanna/course/entities/Order.java
+++ seção 23 - Projeto Web Services com Spring Boot e JPA & Hibernate/course/src/main/java/com/giovanna/course/entities/Order.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.time.Instant;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.giovanna.course.entities.enums.OrderStatus;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -27,6 +28,9 @@ public class Order implements Serializable{
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    // ENUM
+    private Integer orderStatus;
+
     // associação
     @ManyToOne
     @JoinColumn(name = "client_id")
@@ -36,9 +40,10 @@ public class Order implements Serializable{
     public Order() {
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);;
         this.client = client;
     }
 
@@ -57,6 +62,16 @@ public class Order implements Serializable{
 
     public void setMoment(Instant moment) {
         this.moment = moment;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+        this.orderStatus = orderStatus.getCode();
+        }
     }
 
     public User getClient() {
@@ -92,6 +107,4 @@ public class Order implements Serializable{
             return false;
         return true;
     }
-
-    
 }
---
+++ seção 23 - Projeto Web Services com Spring Boot e JPA & Hibernate/course/src/main/java/com/giovanna/course/entities/enums/OrderStatus.java
@@ -0,0 +1,33 @@
+package com.giovanna.course.entities.enums;
+
+public enum OrderStatus {
+    WAITING_PAYMENT(1), // Aparece o numero 1 no banco de dados quando usado esse enum
+    PAID(2),
+    SHIPPED(3),
+    DELIVERED(4),
+    CANCELED(5);
+
+    // código do tipo enumerado
+    private int code; 
+
+    private OrderStatus(int code) {
+        this.code = code;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    // os métodos estáticos não são preciso instanciar 
+    public static OrderStatus valueOf(int code) {
+
+        // Percorre todos os valores possíveis do OrderStatus, e pra cada um deles é testado o código do parâmetro é correpondente ao enum
+        for(OrderStatus value: OrderStatus.values()){
+            if (value.getCode() == code) {
+                return value;
+            }
+        }
+        // exceção para se o if varrer todos os valores possíveis e nenhum for igual ao parâmetro inserido
+        throw new IllegalArgumentException("Invalid OrderStatus code");
+    }
+}
