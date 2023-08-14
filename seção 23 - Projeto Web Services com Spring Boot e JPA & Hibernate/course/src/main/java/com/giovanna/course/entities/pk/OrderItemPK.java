7efdd9e59cc97c4b388fb862881a35efe311978f
Giovanna Federico <giovannamfederico@gmail.com>
2023-08-14 14:30:29 -0300

    OrderItem, many-to-many association with extra attributes PART 1

---
+++ seção 23 - Projeto Web Services com Spring Boot e JPA & Hibernate/course/src/main/java/com/giovanna/course/entities/pk/OrderItemPK.java
@@ -0,0 +1,71 @@
+package com.giovanna.course.entities.pk;
+// essa classe terá uma referência para o produto e o pedido
+
+import java.io.Serializable;
+
+import com.giovanna.course.entities.Order;
+import com.giovanna.course.entities.Product;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+@Embeddable // classe auxiliar de chave primária compposta
+public class OrderItemPK implements Serializable{
+    
+    // atributos
+    // Muitos para um
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    // getters e setters
+    public Order getOrder() {
+        return order;
+    }
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+    public Product getProduct() {
+        return product;
+    }
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    // hashCode e Equals do order e product
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((order == null) ? 0 : order.hashCode());
+        result = prime * result + ((product == null) ? 0 : product.hashCode());
+        return result;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        OrderItemPK other = (OrderItemPK) obj;
+        if (order == null) {
+            if (other.order != null)
+                return false;
+        } else if (!order.equals(other.order))
+            return false;
+        if (product == null) {
+            if (other.product != null)
+                return false;
+        } else if (!product.equals(other.product))
+            return false;
+        return true;
+    }
+
+    
+}
