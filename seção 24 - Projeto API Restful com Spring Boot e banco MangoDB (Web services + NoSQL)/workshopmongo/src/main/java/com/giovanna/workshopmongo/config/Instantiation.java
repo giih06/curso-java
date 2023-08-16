be9be62ca85c554bb757359506950045116d9c1a
Giovanna Federico <giovannamfederico@gmail.com>
2023-08-16 19:52:31 -0300

    Operação de instanciação da base de dados

---
+++ seção 24 - Projeto API Restful com Spring Boot e banco MangoDB (Web services + NoSQL)/workshopmongo/src/main/java/com/giovanna/workshopmongo/config/Instantiation.java
@@ -0,0 +1,31 @@
+package com.giovanna.workshopmongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.giovanna.workshopmongo.domain.User;
+import com.giovanna.workshopmongo.repository.UserRepository;
+
+// Essa classe instancia os dados da tabela toda vez que rodar a aplicação
+@Configuration
+public class Instantiation implements CommandLineRunner{
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        
+        userRepository.deleteAll();
+
+        User maria = new User(null, "Maria Brown", "maria@gmail.com");
+        User alex = new User(null, "Alex Green", "alex@gmail.com");
+        User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+        userRepository.saveAll(Arrays.asList(maria, alex, bob));
+    }
+    
+}
