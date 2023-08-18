642e5d3e7a225dff85d83b0bde820e44232151ed
Giovanna Federico <giovannamfederico@gmail.com>
2023-08-18 10:30:31 -0300

    Consulta simples com query methods

--- seção 24 - Projeto API Restful com Spring Boot e banco MangoDB (Web services + NoSQL)/workshopmongo/src/main/java/com/giovanna/workshopmongo/services/PostService.java
+++ seção 24 - Projeto API Restful com Spring Boot e banco MangoDB (Web services + NoSQL)/workshopmongo/src/main/java/com/giovanna/workshopmongo/services/PostService.java
@@ -1,10 +1,10 @@
 package com.giovanna.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.giovanna.workshopmongo.domain.Post;
 import com.giovanna.workshopmongo.repository.PostRepository;
 import com.giovanna.workshopmongo.services.exception.ObjectNotFoundException;
@@ -15,8 +15,14 @@ public class PostService {
 	@Autowired
 	private PostRepository repo;
 
+    // Encontra por id
 	public Post findById(String id) {
 		Optional<Post> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+    // Consulta String ignorando maiúsculas e minuúsculas
+    public List<Post> findByTitle(String text) {
+		return repo.findByTitleContainingIgnoreCase(text);
 	}
 }
